1cfc8e9be0a2d345a7d5aa4ccb5747598809759e
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-09-19 22:51:37 +1000

    fixes to buttons

---
+++ content_scripts/linkly.js
@@ -0,0 +1,28 @@
+function linkly(request, sender, sendResponse) {
+  console.log("hi");
+  switch (request.linklyChoice) {
+    case "HTML":
+      removeEverything();
+      linklyHtml(request.linklyURL);
+      break;
+    case "Console":
+      linklyConsole(request.linklyURL);
+      break;
+  }
+}
+
+function removeEverything() {
+  while (document.body.firstChild) {
+    document.body.firstChild.remove();
+  }
+}
+function linklyHtml(linklyURL) {
+  var linklyText = document.createTextNode(linklyURL);
+  document.body.appendChild(linklyText);
+}
+
+function linklyConsole(linklyURL) {
+  chrome.extension.getBackgroundPage().console.log(linklyURL);
+}
+
+chrome.runtime.onMessage.addListener(linkly);
---
+++ popup/linkly_send.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", function(e) {
+  if (!e.target.classList.contains("click_option")) {
+    return;
+  }
+  var linklyTab = "www.thegooges.com";
+  var linklyChoice = e.target.textContent;
+  chrome.tabs.executeScript(null, {
+    file: "/content_scripts/linkly.js"
+  });
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {linklyURL: linklyTab, linklyChoice: linklyChoice});
+  });
+});
